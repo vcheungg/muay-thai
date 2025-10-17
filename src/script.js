6837c059bc6271605fe47183fc9858276f91e4f5
vcheungg <vivianncheungg@gmail.com>
2025-10-17 13:57:31 -0700

    added functionalities/forms/event listeners/api ai integration

---
+++ src/script.js
@@ -0,0 +1,16 @@
+function generateCombo(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#combo-idea");
+  let apiKey = "047t2173e3a39c66942c701baf3a6of5";
+  let prompt = `User inputs: Generate a 6-12 hit combo starting with ${userInput.value}`;
+  let context =
+    "Follow user prompt - start a muay thai combo with user input value, can include jab, cross, knees, teeps, elbows, kicks of any kind, right or left side and make it flow";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+}
+
+let muaythaiCombo = document.querySelector("#combo");
+muaythaiCombo.innerHTML = "Generating a combo..";
+axios.get(apiUrl).then(displayCombo);
+
+let comboForm = document.querySelector("#combo-generator");
+comboForm.addEventListener("submit", generateCombo);
